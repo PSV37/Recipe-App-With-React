fb55434d94107fe3216f7328a60136c9ee8521d7
unknown <sapnapastapure@gmail.com>
2020-02-22 08:13:36 -0800

    'create_new_recipe_app_with_react'

--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
+import Recipe from './Recipe'
 import './App.css';
+import loading from './loading.png';
 
-function App() {
+const App = () =>  {
+  const APP_ID = "4e32c3fe"
+  const APP_KEY = "88f56d507ec635e8c0741cfc52fbc635"
+
+  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState('');
+  const [query, setQuery] = useState('chicken');
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    
+    getRecipes();
+   
+  }, [query]);
+
+
+  const getRecipes = async () => {
+    setIsLoading(true)
+    const res = await fetch(
+      `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
+      );
+
+    setIsLoading(false)
+    const data = await res.json();
+    setRecipes(data.hits)
+    console.log(data.hits);
+  }
+
+
+  const updateSearch = (e) => {
+    setSearch(e.target.value)
+  }
+
+
+  const getSearch = (e) => {
+    setIsLoading(true)
+    e.preventDefault();
+    setQuery(search)
+    setSearch('')
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={getSearch} className="search-form">
+        <div className="form-group " style={{display:"-webkit-box"}}>
+          <input type="text" className="form-control search-bar" value={search} onChange={updateSearch} placeholder="Search new recipes here"/>
+          <button className="search-button btn btn-primary" type="submit">SEARCH</button>
+        </div>
+      </form>
+      <div className="recipes">
+      {isLoading ? (<div className="text-center" style={{marginRight:'50px'}}><h2>Loading...</h2></div>) : null}
+        {recipes.map((recipe) =>(
+              <Recipe 
+                key={recipe.recipe.label}
+                title={recipe.recipe.label}  
+                calories={recipe.recipe.calories}  
+                image={recipe.recipe.image} 
+                ingredients={recipe.recipe.ingredients}
+                />
+            ))
+        }
+      </div>
+
     </div>
   );
 }
